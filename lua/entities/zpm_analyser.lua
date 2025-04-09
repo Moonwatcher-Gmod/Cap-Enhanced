654efe9c0fe12903d37e176e0655a9b7ae25fe15
NovaAstral <mariofluddd@gmail.com>
2025-04-08 23:20:55 -0300

    ZPM Analyzer Wires and CFD code fix - Added Wire inputs and outputs to the ZPM Analyzer Mk1 and Mk2 - Fixed the CFD checking for the code after it generated it, so it was impossible to unlock unless you knew what code it was about to generate - Fixed the ZPM Analyzer Mk2 not being able to eject ZPM's - Fixed a typo and bad grammar in a 'You're not an admin' print - Converted spaces to tabs so hopefully github wont be mad

--- lua/entities/zpm_analyser.lua
+++ lua/entities/zpm_analyser.lua
@@ -11,15 +11,17 @@ if (SGLanguage!=nil and SGLanguage.GetMessage!=nil) then
 ENT.Category = SGLanguage.GetMessage("entity_main_cat");
 end
 list.Set("CAP.Entity", ENT.PrintName, ENT)
-
+if (StarGate ~= nil and StarGate.LifeSupportAndWire ~= nil) then
+	StarGate.LifeSupportAndWire(ENT)
+end
 
 if SERVER then
 	AddCSLuaFile()
 
 	ENT.Sounds = {
-        Place = Sound("tech/comstone_placestone.wav"),
-        Transfer = Sound("tech/comstone_transferminds.wav")
-    }
+		Place = Sound("tech/comstone_placestone.wav"),
+		Transfer = Sound("tech/comstone_transferminds.wav")
+	}
 
 	function ENT:Initialize()
 		self.Entity:SetModel("models/madman07/com_device/device.mdl");
@@ -39,6 +41,9 @@ if SERVER then
 		self.Time = 0
 		self.Complete = 0
 		self.ZPM = nil
+
+		self:CreateWireInputs("Activate")
+		self:CreateWireOutputs("Active","ZPM [ENTITY]","Progress %","Tainted")
 	end
 
 	function ENT:SpawnFunction(p,t)
@@ -57,39 +62,57 @@ if SERVER then
 		return e;
 	end
 
+	function ENT:TurnOn()
+		self.Entity:SetNWBool("Active", true)
+		self.Sound:PlayEx(0.5,70)
+		self.Active = true
+		self.Entity:SetNWInt("Result",0)
+		
+		self:SetWire("Active",1)
+		self:SetWire("Tainted",0)
+	end
+
+	function ENT:TurnOff()
+		self.Entity:SetNWBool("Active", false)
+		self.Sound:Stop()
+		self.Active = false
+
+		self:SetWire("Active",0)
+	end
+
 	function ENT:Use(ply)
-		if (self.Active) then
-			self.Entity:SetNWBool("Active", false)
-			self.Sound:Stop();
-			self.Active = false
+		if(self.Active) then
+			self:TurnOff()
 		else
-			if self.InContact then
-				self.Entity:SetNWBool("Active", true)
-				self.Sound:PlayEx(0.5,70);
-				self.Active = true
-				self.Entity:SetNWInt("Result",0)
+			if(self.InContact) then
+				self:TurnOn()
 			end
 		end
 	end
 
 	function ENT:StartTouch(ent)
-		if (ent:GetClass() == "zpm_mk3") then
+		if(ent:GetClass() == "zpm_mk3") then
 			self:EmitSound(self.Sounds.Place, 100, math.random(98, 102))
 			self.InContact = true
 			self.ZPM = ent
 			self.Entity:SetNWInt("Percentage", self.Percentcomp)
+
+			self:SetWire("ZPM",ent)
+			self:SetWire("Progress %",self.Percentcomp)
 		end
 	end
 
 	function ENT:EndTouch(ent)
-		if (ent:GetClass() == "zpm_mk3") then
-			self.Entity:SetNWBool("Active", false)
+		if(ent:GetClass() == "zpm_mk3") then
+			self:TurnOff()
 			self.InContact = false
-			self.Sound:Stop();
-			self.Active = false
 			self.Percentcomp = 0
 			self.ZPM = nil
 			self.Entity:SetNWInt("Percentage", self.Percentcomp)
+
+			self:SetWire("ZPM",Entity(0))
+			self:SetWire("Progress %",self.Percentcomp)
+			self:SetWire("Tainted",0)
 		end
 	end
 
@@ -99,20 +122,37 @@ if SERVER then
 				self.Percentcomp = math.Clamp(self.Percentcomp + math.floor(math.Rand(1, 10)) , 0, 100) 
 				self.Time = CurTime() + 1
 			end
+			
 			self.Entity:SetNWInt("Percentage", self.Percentcomp)
+			self:SetWire("Progress %",self.Percentcomp)
+
 			if self.Percentcomp == 100 then
-				self.Active = false
 				self.InContact = false
-				self.Entity:SetNWBool("Active", false)
-				self.Sound:Stop();
+				self:TurnOff()
+
 				if (self.ZPM.IsTampered) then
 					self.Entity:SetNWInt("Result",1)
+					self:SetWire("Tainted",1)
 					self:EmitSound("sg/scanner/deactivate2.wav")
 				else
 					self.Entity:SetNWInt("Result",2)
+					self:SetWire("Tainted",0)
 					self:EmitSound("sg/scanner/deactivate2.wav")
 				end
+			end
+		end
+	end
 
+	function ENT:TriggerInput(iname,value)
+		if(iname == "Activate") then
+			if(value >= 1) then
+				if(self.InContact and self.Active == false) then
+					self:TurnOn()
+				end
+			else
+				if(self.Active) then
+					self:TurnOff()
+				end
 			end
 		end
 	end
@@ -123,93 +163,93 @@ if CLIENT then
 
 	ENT.Device_hud = surface.GetTextureID("VGUI/resources_hud/tier_2")
 	function ENT:Draw()
-        self.Entity:DrawModel()
-        hook.Remove("HUDPaint", tostring(self.Entity) .. "zpm_a")
+		self.Entity:DrawModel()
+		hook.Remove("HUDPaint", tostring(self.Entity) .. "zpm_a")
 
-        if (LocalPlayer():GetEyeTrace().Entity == self.Entity and EyePos():Distance(self.Entity:GetPos()) < 1024) then
-            hook.Add("HUDPaint", tostring(self.Entity) .. "zpm_a", function()
-            	local w = 0
-	            local h = 260
-                surface.SetTexture(self.Device_hud)
-                surface.SetDrawColor(Color(255, 255, 255, 155))
-                surface.DrawTexturedRect(ScrW() / 2 + 6 + w, ScrH() / 2 - 50 - h, 200, 360)
-                local chann = 1
+		if (LocalPlayer():GetEyeTrace().Entity == self.Entity and EyePos():Distance(self.Entity:GetPos()) < 1024) then
+			hook.Add("HUDPaint", tostring(self.Entity) .. "zpm_a", function()
+				local w = 0
+				local h = 260
+				surface.SetTexture(self.Device_hud)
+				surface.SetDrawColor(Color(255, 255, 255, 155))
+				surface.DrawTexturedRect(ScrW() / 2 + 6 + w, ScrH() / 2 - 50 - h, 200, 360)
+				local chann = 1
 
-                if IsValid(self.Entity) then
-                    chann = self.Entity:GetNetworkedInt("Chann", 1)
-                end
+				if IsValid(self.Entity) then
+					chann = self.Entity:GetNetworkedInt("Chann", 1)
+				end
 
-                local act = false
-                local percent = 0
-                local result = 0
+				local act = false
+				local percent = 0
+				local result = 0
 
-                if IsValid(self.Entity) then
-                    act = self.Entity:GetNWBool("Active", false)
-                    percent = self.Entity:GetNWInt("Percentage",0)
-                    result = self.Entity:GetNWInt("Result",0)
-                end
-                draw.DrawText("Analyser", "HudHintTextLarge", ScrW() / 2 + 55, ScrH() / 2 -223, Color(0, 255, 255, 255), 0)
-                
-                if (act) then
-                	draw.DrawText("Active", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 200, Color(0, 238, 38, 255), 0)
-            	else
-            		draw.DrawText("Offline", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 200, Color(255, 38, 38, 255), 0)
-            	end
-                
-                draw.DrawText(percent.." %", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 175, Color(209, 238, 238, 255), 0)
+				if IsValid(self.Entity) then
+					act = self.Entity:GetNWBool("Active", false)
+					percent = self.Entity:GetNWInt("Percentage",0)
+					result = self.Entity:GetNWInt("Result",0)
+				end
+				draw.DrawText("Analyser", "HudHintTextLarge", ScrW() / 2 + 55, ScrH() / 2 -223, Color(0, 255, 255, 255), 0)
+				
+				if (act) then
+					draw.DrawText("Active", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 200, Color(0, 238, 38, 255), 0)
+				else
+					draw.DrawText("Offline", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 200, Color(255, 38, 38, 255), 0)
+				end
+				
+				draw.DrawText(percent.." %", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 175, Color(209, 238, 238, 255), 0)
 
-                draw.DrawText("Result:", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 150, Color(209, 238, 238, 255), 0)
+				draw.DrawText("Result:", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 150, Color(209, 238, 238, 255), 0)
 
-                if (result==1) then
-                	draw.DrawText("Variance detected!", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 130, Color(255, 38, 38, 255), 0)
-                elseif (result==2) then
-                	draw.DrawText("Stable", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 130, Color(38, 255, 38, 255), 0)
-                end
-            end)
-        end
+				if (result==1) then
+					draw.DrawText("Variance detected!", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 130, Color(255, 38, 38, 255), 0)
+				elseif (result==2) then
+					draw.DrawText("Stable", "HudHintTextLarge", ScrW() / 2 + 35, ScrH() / 2 - 130, Color(38, 255, 38, 255), 0)
+				end
+			end)
+		end
 
-        local Online = self:GetNWBool("Active")
-        --self:DrawModel()
+		local Online = self:GetNWBool("Active")
+		--self:DrawModel()
 
-        if (Online) then
-            self:DynLight(true)
-        elseif ((not (Online))) then
-            self:DynLight(false)
-        end
-
-
-
-
-    end
-
-
-    function ENT:DynLight()
-
-        local pos = self:GetPos() + self:GetUp() * 15 + self:GetUp() * (math.sin(CurTime()*2)*5)
-        local pos2 = self:GetPos() + self:GetUp() * 2
-        local Online = self:GetNWBool("Active",false)
-
-
-        if (IsValid(self)) then
-            if (Online) then
-                local dynlight = DynamicLight(self:EntIndex() + 4096)
-                dynlight.Pos = pos
-                dynlight.Brightness = 15
-                dynlight.Size = 14
-                dynlight.Decay = 1024
-                dynlight.R = 25
-                dynlight.G = 255
-                dynlight.B = 255
-                dynlight.DieTime = CurTime() + 1
-            end
-        end
-    end
+		if (Online) then
+			self:DynLight(true)
+		elseif ((not (Online))) then
+			self:DynLight(false)
+		end
 
 
 
-    function ENT:OnRemove()
-        hook.Remove("HUDPaint", tostring(self.Entity) .. "zpm_a")
-    end
+
+	end
+
+
+	function ENT:DynLight()
+
+		local pos = self:GetPos() + self:GetUp() * 15 + self:GetUp() * (math.sin(CurTime()*2)*5)
+		local pos2 = self:GetPos() + self:GetUp() * 2
+		local Online = self:GetNWBool("Active",false)
+
+
+		if (IsValid(self)) then
+			if (Online) then
+				local dynlight = DynamicLight(self:EntIndex() + 4096)
+				dynlight.Pos = pos
+				dynlight.Brightness = 15
+				dynlight.Size = 14
+				dynlight.Decay = 1024
+				dynlight.R = 25
+				dynlight.G = 255
+				dynlight.B = 255
+				dynlight.DieTime = CurTime() + 1
+			end
+		end
+	end
+
+
+
+	function ENT:OnRemove()
+		hook.Remove("HUDPaint", tostring(self.Entity) .. "zpm_a")
+	end
 
 
 end
